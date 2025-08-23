ea4d1c3cb211c31b0f3b0c556315bd4361aec7ff
Zyad Salah <salahzyad066@gmail.com>
2025-08-23 18:06:37 +0300

    Update Assigments(38-46).cpp

--- C++ Assigments(38-46)/Assigments(38-46).cpp
+++ C++ Assigments(38-46)/Assigments(38-46).cpp
@@ -1,290 +1,290 @@
-// "Number 100 Award Is: iPhone"
-// "Number 300 Award Is: iPad"
-// "Number 600 Award Is: PC"
-// "Number 900 Award Is: Car"
+// // "Number 100 Award Is: iPhone"
+// // "Number 300 Award Is: iPad"
+// // "Number 600 Award Is: PC"
+// // "Number 900 Award Is: Car"
 
 
-// #include <iostream>
-// #include <array>
-// #include <string.h>
+// // #include <iostream>
+// // #include <array>
+// // #include <string.h>
 
-// using namespace std;
-// int main()
-// {
-//     // Assigment Number[1]
-    // int nums[]{100, 300, 600, 900};
-    // string awards[]{"iPhone", "iPad", "PC", "Car"};
-    // cout <<  "Number " <<   nums[0] << " Award Is : " << awards[0]  << endl ;  
-    // cout <<  "Number " <<   nums[1] << " Award Is : " << awards[1]  << endl; 
-    // cout <<  "Number " <<   nums[2] << " Award Is : " << awards[2]  << endl; 
-    // cout <<  "Number " <<   nums[3] << " Award Is : " << awards[3]  << endl; 
+// // using namespace std;
+// // int main()
+// // {
+// //     // Assigment Number[1]
+//     // int nums[]{100, 300, 600, 900};
+//     // string awards[]{"iPhone", "iPad", "PC", "Car"};
+//     // cout <<  "Number " <<   nums[0] << " Award Is : " << awards[0]  << endl ;  
+//     // cout <<  "Number " <<   nums[1] << " Award Is : " << awards[1]  << endl; 
+//     // cout <<  "Number " <<   nums[2] << " Award Is : " << awards[2]  << endl; 
+//     // cout <<  "Number " <<   nums[3] << " Award Is : " << awards[3]  << endl; 
 
-    // Assigment Number[2]
-    // Second Array
-//     int check = 25;
-//     int nums[]{20, 35, 30, 70, 100};
-//     // int nums[]{40, 20, 30, 70, 100};
-//     if( nums[0]> check )
-//     {
-//         cout << nums[0] + nums[3] << endl ;
-//         // Nasted If 1
-//         // if(nums[1] > check  )
-//         // {   
-//         //     cout << nums[1] + nums[3] << endl;
-//         // } else if(nums[2] > check )
-//         // {
-//         //     cout << nums[2] + nums[3] << endl; 
-//         // }
-//     } else if (nums[1] > check)
-//     {
-//     cout << nums[1] + nums[3] << endl;  
-//     } if (nums[2] > check)
-//     {
-//         cout << nums[2] + nums[3] << endl; 
+//     // Assigment Number[2]
+//     // Second Array
+// //     int check = 25;
+// //     int nums[]{20, 35, 30, 70, 100};
+// //     // int nums[]{40, 20, 30, 70, 100};
+// //     if( nums[0]> check )
+// //     {
+// //         cout << nums[0] + nums[3] << endl ;
+// //         // Nasted If 1
+// //         // if(nums[1] > check  )
+// //         // {   
+// //         //     cout << nums[1] + nums[3] << endl;
+// //         // } else if(nums[2] > check )
+// //         // {
+// //         //     cout << nums[2] + nums[3] << endl; 
+// //         // }
+// //     } else if (nums[1] > check)
+// //     {
+// //     cout << nums[1] + nums[3] << endl;  
+// //     } if (nums[2] > check)
+// //     {
+// //         cout << nums[2] + nums[3] << endl; 
     
     
-// } 
+// // } 
 
-    // Assigment [3]
+//     // Assigment [3]
 
-    // int filling = 10;
-    // int vals[]{100, 200, 300, 400};
+//     // int filling = 10;
+//     // int vals[]{100, 200, 300, 400};
 
-    // Write One Line Of Code Here To Fill Array Value With Number 10
-    // vals[0]=vals[1]=vals[2]=vals[3] =10;
-    // cout << vals[0] << "\n"; // 10
-    // cout << vals[1] << "\n"; // 10
-    // cout << vals[2] << "\n"; // 10
-    // cout << vals[3] << "\n"; // 10
+//     // Write One Line Of Code Here To Fill Array Value With Number 10
+//     // vals[0]=vals[1]=vals[2]=vals[3] =10;
+//     // cout << vals[0] << "\n"; // 10
+//     // cout << vals[1] << "\n"; // 10
+//     // cout << vals[2] << "\n"; // 10
+//     // cout << vals[3] << "\n"; // 10
 
-    // Assigment [4]
-    // int vals[]{100, 200, 250, 400, 200};
-    // int vals[]{100, 200, 500, 400, 200};
-    // int vals[]{100, 200, 600, 400, 200};
-
-
-    // cout << sizeof(vals)/ sizeof(vals[0]) << endl;
-
-    // if( vals[0]+vals[4] > vals[2])
-    // {
-    //     cout << "\"First number + Last Number Is Large Than middle Number \"" << endl ;
-    //     cout <<  "\"" << vals[0] << " + " << vals[4] << " = " << vals[0] + vals[4] <<"\"" << endl;
-    //     cout << "\" "<<   vals[0]+vals[4] << " > "<<  vals[2]  << "\"" << endl ;
-    // } else if( vals[1]+vals[3] > vals[2])
-    // {
-    //     cout << "\"Second Number +  Before Last Number Is Large Than middle Number \"" << endl ;
-    //     cout <<  "\"" << vals[1] << " + " << vals[3] << " = " << vals[1] + vals[3] <<"\"" << endl;
-    //     cout << "\" "<<   vals[1]+vals[3] << " > "<<  vals[2]  << "\"" << endl ;
-    // } else
-    // {
-    //     cout << "Middle Number Is The Largest " << endl;
-    //     cout << "\""<<vals[2] << "\"" ;
-    // }
-
-    // Assigment [5]
-
-    // int vals[] = {100, 200, 600, 200, 100};
-
-    // switch(vals[0] == vals[4] && vals[1]== vals[3])
-    // {
-    //     case 1: 
-    //     cout << "\"Array Is Palindrome\"" <<  endl;
-    //     break;
-    //     default :
-    //     cout << "\"Array Is not  Palindrome\"" << endl;
-    // }
+//     // Assigment [4]
+//     // int vals[]{100, 200, 250, 400, 200};
+//     // int vals[]{100, 200, 500, 400, 200};
+//     // int vals[]{100, 200, 600, 400, 200};
 
 
-    // int vals_2[] = {100, 200, 200, 100};
+//     // cout << sizeof(vals)/ sizeof(vals[0]) << endl;
 
-    // switch(vals_2[0] == vals_2[3] && vals_2[1]== vals_2[2])
-    // {
-    //     case 1: 
-    //     cout << "\"Array Is Palindrome\"" << endl;
-    //     break;
-    //     default :
-    //     cout << "\"Array Is not  Palindrome\"" << endl;}
+//     // if( vals[0]+vals[4] > vals[2])
+//     // {
+//     //     cout << "\"First number + Last Number Is Large Than middle Number \"" << endl ;
+//     //     cout <<  "\"" << vals[0] << " + " << vals[4] << " = " << vals[0] + vals[4] <<"\"" << endl;
+//     //     cout << "\" "<<   vals[0]+vals[4] << " > "<<  vals[2]  << "\"" << endl ;
+//     // } else if( vals[1]+vals[3] > vals[2])
+//     // {
+//     //     cout << "\"Second Number +  Before Last Number Is Large Than middle Number \"" << endl ;
+//     //     cout <<  "\"" << vals[1] << " + " << vals[3] << " = " << vals[1] + vals[3] <<"\"" << endl;
+//     //     cout << "\" "<<   vals[1]+vals[3] << " > "<<  vals[2]  << "\"" << endl ;
+//     // } else
+//     // {
+//     //     cout << "Middle Number Is The Largest " << endl;
+//     //     cout << "\""<<vals[2] << "\"" ;
+//     // }
 
-    // int vals_3[] = {100, 300, 600, 200, 100};
+//     // Assigment [5]
 
-    // switch(vals_3[0] == vals_3[4] && vals_3[1]== vals_3[3])
-    // {
-    //     case 1: 
-    //     cout << "\"Array Is Palindrome\"" << endl;
-    //     break;
-    //     default :
-    //     cout << "\"Array Is not  Palindrome\"" << endl;
-    // }
+//     // int vals[] = {100, 200, 600, 200, 100};
 
-    // Assigment [6]
-
-    // int vals[] = {10, 20, 30};
-
-    // vals[0]  = 100 ;
-    // vals[1]  = 200 ;
-    // vals[2]  = 300 ; 
-
-    // Another Soloution 
-
-    // vals[0] = vals[0]*10;
-    // vals[1] = vals[1]*10;
-    // vals[2] = vals[2]*10;
-    //Assigment Number[7]
-
-    // cout << vals[0] << "\n"; // 100
-    // cout << vals[1] << "\n"; // 200
-    // cout << vals[2] << "\n"; // 300
-
-    //   string names[3][3] = {
-    //   {"Ahmed","Sayed","Mahmoud"},
-    //   {"Sameh","Mahdey","Gamal"},
-    //   {"Mohamed" , "Adel" ,"Majed"},
-    //   };
-    //   // Create The Array Here
-
-    // cout << "First Collection Of Names:\n";
-    // cout << names[0][0] << "\n"; // Ahmed
-    // cout << names[1][1] << "\n"; // Mahdy
-    // cout << names[2][2] << "\n"; // Majed
-
-    // cout << "Second Collection Of Names:\n";
-    // cout << names[2][1] << "\n"; // Adel
-    // cout << names[1][2] << "\n"; // Gamal
-    // cout << names[0][2] << "\n"; // Mahmoud
-
-    // cout << "Third Collection Of Names:\n";
-    // cout << names[0][1] << "\n"; // Sayed
-    // cout << names[1][0] << "\n"; // Sameh
-    // cout << names[2][0] << "\n"; // Mohamed
+//     // switch(vals[0] == vals[4] && vals[1]== vals[3])
+//     // {
+//     //     case 1: 
+//     //     cout << "\"Array Is Palindrome\"" <<  endl;
+//     //     break;
+//     //     default :
+//     //     cout << "\"Array Is not  Palindrome\"" << endl;
+//     // }
 
 
+//     // int vals_2[] = {100, 200, 200, 100};
 
-        // Assigment [8]
+//     // switch(vals_2[0] == vals_2[3] && vals_2[1]== vals_2[2])
+//     // {
+//     //     case 1: 
+//     //     cout << "\"Array Is Palindrome\"" << endl;
+//     //     break;
+//     //     default :
+//     //     cout << "\"Array Is not  Palindrome\"" << endl;}
+
+//     // int vals_3[] = {100, 300, 600, 200, 100};
+
+//     // switch(vals_3[0] == vals_3[4] && vals_3[1]== vals_3[3])
+//     // {
+//     //     case 1: 
+//     //     cout << "\"Array Is Palindrome\"" << endl;
+//     //     break;
+//     //     default :
+//     //     cout << "\"Array Is not  Palindrome\"" << endl;
+//     // }
+
+//     // Assigment [6]
+
+//     // int vals[] = {10, 20, 30};
+
+//     // vals[0]  = 100 ;
+//     // vals[1]  = 200 ;
+//     // vals[2]  = 300 ; 
+
+//     // Another Soloution 
+
+//     // vals[0] = vals[0]*10;
+//     // vals[1] = vals[1]*10;
+//     // vals[2] = vals[2]*10;
+//     //Assigment Number[7]
+
+//     // cout << vals[0] << "\n"; // 100
+//     // cout << vals[1] << "\n"; // 200
+//     // cout << vals[2] << "\n"; // 300
+
+//     //   string names[3][3] = {
+//     //   {"Ahmed","Sayed","Mahmoud"},
+//     //   {"Sameh","Mahdey","Gamal"},
+//     //   {"Mohamed" , "Adel" ,"Majed"},
+//     //   };
+//     //   // Create The Array Here
+
+//     // cout << "First Collection Of Names:\n";
+//     // cout << names[0][0] << "\n"; // Ahmed
+//     // cout << names[1][1] << "\n"; // Mahdy
+//     // cout << names[2][2] << "\n"; // Majed
+
+//     // cout << "Second Collection Of Names:\n";
+//     // cout << names[2][1] << "\n"; // Adel
+//     // cout << names[1][2] << "\n"; // Gamal
+//     // cout << names[0][2] << "\n"; // Mahmoud
+
+//     // cout << "Third Collection Of Names:\n";
+//     // cout << names[0][1] << "\n"; // Sayed
+//     // cout << names[1][0] << "\n"; // Sameh
+//     // cout << names[2][0] << "\n"; // Mohamed
+
+
+
+//         // Assigment [8]
 
     
-  // Your Code Here
+//   // Your Code Here
 
-    // array <int,3> nums ={1,2,3} ;
-    // // Do Not Edit
-    // nums.fill(100);
-    // cout << nums[0] << "\n"; // 100
-    // cout << nums[1] << "\n"; // 100
-    // cout << nums[2] << "\n"; // 100
-
-
-
-    // Assigment [9]
+//     // array <int,3> nums ={1,2,3} ;
+//     // // Do Not Edit
+//     // nums.fill(100);
+//     // cout << nums[0] << "\n"; // 100
+//     // cout << nums[1] << "\n"; // 100
+//     // cout << nums[2] << "\n"; // 100
 
 
-    // int nums[] = {10, 20, 30, 40, 20, 50};
 
-    // // Method 1
-    // cout << sizeof(nums) /sizeof(nums[0]) ;
-    // 6
+//     // Assigment [9]
 
-    // // Method 2
-    // array <int,6> nums ;
-    // cout << nums.size();
-    // 6
 
-    // // Method 3
-    // cout << nums.; // Remember To Search
-    // 6
+//     // int nums[] = {10, 20, 30, 40, 20, 50};
 
-    // Assigment [10]
+//     // // Method 1
+//     // cout << sizeof(nums) /sizeof(nums[0]) ;
+//     // 6
 
-//     array<int, 6> nums = {10, 20, 30, 40, 20, 50};
+//     // // Method 2
+//     // array <int,6> nums ;
+//     // cout << nums.size();
+//     // 6
 
-// //   // Method 1
+//     // // Method 3
+//     // cout << nums.; // Remember To Search
+//     // 6
 
-//     cout << "\"First : " << nums.front() << "\"" << endl;
-//     cout << "\"Last : " << nums.back()  << "\"" << endl;
-//     cout << endl;
-// //   "First: 10" 
+//     // Assigment [10]
+
+// //     array<int, 6> nums = {10, 20, 30, 40, 20, 50};
+
+// // //   // Method 1
+
+// //     cout << "\"First : " << nums.front() << "\"" << endl;
+// //     cout << "\"Last : " << nums.back()  << "\"" << endl;
+// //     cout << endl;
+// // //   "First: 10" 
+// // //   "Last: 50"
+
+// // //   // Method 2
+// //     cout << "\"First : " << nums.at(0) << "\"" << endl;
+// //     cout << "\"Last : " << nums.at(5)<< "\"" << endl;
+//     // cout << endl;
+// //   "First: 10"
 // //   "Last: 50"
 
-// //   // Method 2
-//     cout << "\"First : " << nums.at(0) << "\"" << endl;
-//     cout << "\"Last : " << nums.at(5)<< "\"" << endl;
-    // cout << endl;
-//   "First: 10"
-//   "Last: 50"
+// // //   // Method 3
 
-// //   // Method 3
-
-//     cout << "\"First : " << nums[0] << "\"" << endl;
-//     cout << "\"Last : " << nums.at(6)<< "\"" << endl;
-//     cout << endl;
-// //   "First: 10"
-//   "Last: 50"
+// //     cout << "\"First : " << nums[0] << "\"" << endl;
+// //     cout << "\"Last : " << nums.at(6)<< "\"" << endl;
+// //     cout << endl;
+// // //   "First: 10"
+// //   "Last: 50"
 
 
-    // Assigment [11]
+//     // Assigment [11]
 
 
 
-    // Do Not Edit The Next 3 Lines
-    // int index = 1;
-    // array<int, 3> oldNums = {10, 20, 30};
-    // array<int, 3> newNums;
+//     // Do Not Edit The Next 3 Lines
+//     // int index = 1;
+//     // array<int, 3> oldNums = {10, 20, 30};
+//     // array<int, 3> newNums;
 
-    // // Write Your Code Here
-    // index = oldNums[1];
-    // newNums.at(1)= index;
-    // newNums.front() =oldNums.back();
-    // newNums.back() =oldNums.front();
+//     // // Write Your Code Here
+//     // index = oldNums[1];
+//     // newNums.at(1)= index;
+//     // newNums.front() =oldNums.back();
+//     // newNums.back() =oldNums.front();
 
-    // // Do Not Edit The Next 3 Lines
-    // cout << newNums[0] << "\n"; // 30
-    // cout << newNums[1] << "\n"; // 20
-    // cout << newNums[2] << "\n"; // 10
+//     // // Do Not Edit The Next 3 Lines
+//     // cout << newNums[0] << "\n"; // 30
+//     // cout << newNums[1] << "\n"; // 20
+//     // cout << newNums[2] << "\n"; // 10
 
 
 
-    // Assigment [12]
+//     // Assigment [12]
 
-    // Do Not Edit The Next 6 Lines
-    // int nums[] = {1, 2, 3, 5};
-    // int vals[] = {10, 20, 40, 30, 10, 60};
-    // int i1 = nums[3];
-    // int i2 = nums[0];
-    // int i3 = nums[1];
-    // int i4 = nums[2];
+//     // Do Not Edit The Next 6 Lines
+//     // int nums[] = {1, 2, 3, 5};
+//     // int vals[] = {10, 20, 40, 30, 10, 60};
+//     // int i1 = nums[3];
+//     // int i2 = nums[0];
+//     // int i3 = nums[1];
+//     // int i4 = nums[2];
 
-    // Need To Get Ouput 150
-    // You Can Use Plus Operator Only
+//     // Need To Get Ouput 150
+//     // You Can Use Plus Operator Only
 
-    // Your Code Here
+//     // Your Code Here
 
-    // cout << vals[0] + vals[2] +vals[3] +vals[4] +vals[5]; 
+//     // cout << vals[0] + vals[2] +vals[3] +vals[4] +vals[5]; 
 
-    // Assigment [13]
+//     // Assigment [13]
 
-    // string fName = "Elzero ";
-    // string mName = "Web ";
-    // string lName = "School";
+//     // string fName = "Elzero ";
+//     // string mName = "Web ";
+//     // string lName = "School";
 
-    // // Method 1
+//     // // Method 1
 
-    // cout << fName +mName+  lName  << endl; 
+//     // cout << fName +mName+  lName  << endl; 
 
-    // // Method 2
+//     // // Method 2
 
-    // cout << fName.append(mName) << lName << endl; 
+//     // cout << fName.append(mName) << lName << endl; 
 
-    // // Method 3
+//     // // Method 3
 
-    // cout << fName <<  lName << endl;
-    // // cout << strcat(fName,mName,lName) ;
+     // cout << fName <<  lName << endl;
+//     // // cout << strcat(fName,mName,lName) ;
 
-// // Output Needed
-// Elzero Web School
-// Elzero Web School
-// Elzero Web School
+// // // Output Needed
+// // Elzero Web School
+// // Elzero Web School
+// // Elzero Web School
 
 
      // تم بحمد الله 23\8
+    // return 0;
 
-    return 0;
+ // }   
 
-}   
